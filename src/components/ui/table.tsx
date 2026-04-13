570ff8d5273aeee0fb303fee49fc0114e00e5275
naaa760 <neh6a683@gmail.com>
2026-04-13 21:19:48 +0530

    Make tables feel more like real data grids

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -5,12 +5,17 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-  <div className="relative w-full overflow-auto">
-    <table
-      ref={ref}
-      className={cn("w-full caption-bottom text-sm", className)}
-      {...props}
-    />
+  <div className="relative w-full overflow-hidden rounded-xl border border-zinc-200/90 bg-white/60 shadow-sm dark:border-zinc-700/55 dark:bg-zinc-950/45 dark:shadow-[inset_0_1px_0_0_rgba(255,255,255,0.04)]">
+    <div className="w-full overflow-x-auto">
+      <table
+        ref={ref}
+        className={cn(
+          "w-full caption-bottom border-collapse text-sm",
+          className,
+        )}
+        {...props}
+      />
+    </div>
   </div>
 ));
 Table.displayName = "Table";
@@ -19,7 +24,14 @@ const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props} />
+  <thead
+    ref={ref}
+    className={cn(
+      "border-b border-zinc-200/95 bg-zinc-50/98 dark:border-zinc-800 dark:bg-zinc-900/85 [&_tr]:border-0",
+      className,
+    )}
+    {...props}
+  />
 ));
 TableHeader.displayName = "TableHeader";
 
@@ -29,7 +41,10 @@ const TableBody = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <tbody
     ref={ref}
-    className={cn("[&_tr:last-child]:border-0", className)}
+    className={cn(
+      "[&_tr:last-child]:border-0 [&_tr]:border-b [&_tr]:border-zinc-200/80 dark:[&_tr]:border-zinc-800/70",
+      className,
+    )}
     {...props}
   />
 ));
@@ -42,7 +57,7 @@ const TableRow = React.forwardRef<
   <tr
     ref={ref}
     className={cn(
-      "border-b border-zinc-200 transition-colors hover:bg-zinc-50/50 data-[state=selected]:bg-zinc-100 dark:border-zinc-800 dark:hover:bg-zinc-900/50",
+      "transition-colors hover:bg-zinc-50/95 data-[state=selected]:bg-zinc-100 dark:hover:bg-zinc-800/45 dark:data-[state=selected]:bg-zinc-800/70",
       className,
     )}
     {...props}
@@ -57,7 +72,7 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      "h-12 px-4 text-left align-middle font-medium text-zinc-500 dark:text-zinc-400 [&:has([role=checkbox])]:pr-0",
+      "h-11 px-4 text-left align-middle text-[11px] font-semibold uppercase tracking-[0.1em] text-zinc-500 first:pl-4 last:pr-4 dark:text-zinc-400 [&:has([role=checkbox])]:pr-0",
       className,
     )}
     {...props}
@@ -71,7 +86,10 @@ const TableCell = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <td
     ref={ref}
-    className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
+    className={cn(
+      "px-4 py-3 align-middle text-sm text-zinc-800 first:pl-4 last:pr-4 dark:text-zinc-200 [&:has([role=checkbox])]:pr-0",
+      className,
+    )}
     {...props}
   />
 ));
