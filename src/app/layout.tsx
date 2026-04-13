1c5ff44e916be71a530277a25455bc151e5b12fc
naaa760 <neh6a683@gmail.com>
2026-04-13 11:51:05 +0530

    style(layout): Geist sans on body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="flex min-h-full flex-col">
+      <body className="flex min-h-full flex-col font-sans antialiased">
         <AuthSessionProvider>
           <QueryProvider>
             <SetupBanner />
